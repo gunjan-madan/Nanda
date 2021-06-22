55782c5d81570baca8313188a2ea8fb69c30c03c
gunjan-madan <53206795+gunjan-madan@users.noreply.github.com>
2021-06-22 07:44:39 +0530

    add

---
+++ Module1/5.1.py
@@ -0,0 +1,49 @@
+"""-----------Task 1: Expression Galore ---------------"""
+print(" ")
+print("*** Task 1: ***")
+# Uncomment the statements and click Run:
+# a = int(input("Enter an integer value "))
+# b = float(input("Enter a float  value"))
+# c = int(input("Enter another integer value "))
+# expr =  (a / b) * c
+# print ("result is: ", expr)
+
+#Lets Fix the errors above.abs
+
+"""-------Task 2: Visit to the Stationery Shop -----------"""
+print(" ")
+print("*** Task 2: ***")
+# Ready to write a program using different data types.
+# Let's start by visiting the stationery shop.
+# Ryan has been asked to take care of the accounts at the new stationery shop. 
+# The shop sells the following items:
+# A Pack of Pencils (20 in number) - Rs.75
+# A Pack of BallPoint Pens(5 in number)- Rs.30
+# Long Size Notebook - Rs. 127.50
+# Regular Size Notebook - Rs. 41.40
+# Write a Python program for Ryan, to calculate the total amount,and generate the bill for the customer.
+# pencils=int(input("Enter the number of pencils:"))
+# pens=int(input("Enter the number of pens:"))
+# notebook=int(input("Enter the number of Long Size Notebook:"))
+# regular_notebook=int(input("Enter the number of regualr size notebooks:"))
+# total=(pencils*75 + pens*30 + notebook*127.50 + regular_notebook*41.40)
+# print("This is the total:",total)
+"""-----------Task 3: Speed Conversion ------"""
+print(" ")
+print("*** Task 3: ***")
+# Write a program to convert the speed from km/hr to m/s
+# [Hint:To convert km/hr into m/sec, multiply the number by 5 and then divide it by 18.]
+# speed_km=int(input("Enter the speed in km/hr:"))
+# speed_m=((speed_km * 5) / 18)
+# print("The speed in m/s is:",speed_m)
+
+
+"""-----------Task 4: Area and Perimeter of Circle ------"""
+print(" ")
+print("*** Task 4: ***")
+#Write a program to accept the radius of a circle, and calculate its area and perimeter. 
+#[Hint:Area = 3.14 * (radius) to power of 2    and  Perimeter = 2*3.14*radius]
+radius=int(input("Enter the radius of the circle:"))
+area=(3.14*(radius**2))
+perimeter=(2*3.14*radius)
+print(area,perimeter)
